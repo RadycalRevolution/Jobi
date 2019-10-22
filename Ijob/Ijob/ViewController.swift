f7148d8fff3bd47b6f7922d4c98eada87fb366ac
RadycalRevolution <56895244+RadycalRevolution@users.noreply.github.com>
2019-10-22 15:48:14 -0500

    Add files via upload

---
+++ Ijob/Ijob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ijob
+//
+//  Created by UDLAP08 on 10/16/19.
+//  Copyright © 2019 Devlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
